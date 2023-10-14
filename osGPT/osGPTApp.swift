df43817092928ff278dcc611b8cbe68bdc1d70e6
Jamil Dhanani <jamil.dhanani@gmail.com>
2023-10-14 14:11:16 -0700

    Initial Commit

---
+++ osGPT/osGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  osGPTApp.swift
+//  osGPT
+//
+//  Created by Jamil Dhanani on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct osGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
